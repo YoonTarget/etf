9e58a1591581b64c9ef0dc3c1cd332aad31b5d68
ygh <igeek437@unpl.co.kr>
2025-02-10 19:33:29 +0900

    Fix : WebClient 인코딩 모드 NONE으로 변경

--- src/main/java/com/newproject/etf/config/WebClientConfig.java
+++ src/main/java/com/newproject/etf/config/WebClientConfig.java
@@ -3,11 +3,15 @@ package com.newproject.etf.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.DefaultUriBuilderFactory;
 
 @Configuration
 public class WebClientConfig {
+    private final DefaultUriBuilderFactory factory = new DefaultUriBuilderFactory();
+
     @Bean
     public WebClient webClient() {
-        return WebClient.builder().build();
+        factory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.NONE);
+        return WebClient.builder().uriBuilderFactory(factory).build();
     }
 }

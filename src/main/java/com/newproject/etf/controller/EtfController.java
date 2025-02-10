f76543a04f4713d9a1004c3e59b7c5f7b8f79836
ygh <igeek437@unpl.co.kr>
2025-02-10 19:34:42 +0900

    Fix : 응답 데이터 JSON으로 설정

--- src/main/java/com/newproject/etf/controller/EtfController.java
+++ src/main/java/com/newproject/etf/controller/EtfController.java
@@ -10,14 +10,15 @@ public class EtfController {
     private final EtfService etfService;
     private final String url = "https://apis.data.go.kr/1160100/service/GetSecuritiesProductInfoService";
     private final String serviceKey = "BBDYHxpLb5iDQfFrXs95dcZqTnYTBG%2B%2Bo6bPr0BC9bmIHnG5gB48wToN04d4DM8uRSj7m5ha1mQvRdLJ%2Fpss9Q%3D%3D";
+//    private final String serviceKey = "BBDYHxpLb5iDQfFrXs95dcZqTnYTBG++o6bPr0BC9bmIHnG5gB48wToN04d4DM8uRSj7m5ha1mQvRdLJ/pss9Q==";
 
     public EtfController(EtfService etfService) {
         this.etfService = etfService;
     }
 
-    @GetMapping("/{apiName}")
+    @GetMapping(value = "/{apiName}", produces = "application/json")
     public String getPriceInfo(Model model, @PathVariable("apiName") String apiName) {
-        model.addAttribute("resultList", etfService.list(url, serviceKey, apiName));
-        return "priceInfo";
+//        model.addAttribute("resultList", etfService.list(url, serviceKey, apiName));
+        return etfService.list(url, serviceKey, apiName);
     }
 }

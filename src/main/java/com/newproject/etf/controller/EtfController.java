d482ab8505efa3564e20a29497ae2aea1115683d
ygh <igeek437@unpl.co.kr>
2025-02-25 22:57:59 +0900

    Modify : JSON 활용 방식 수정중

--- src/main/java/com/newproject/etf/controller/EtfController.java
+++ src/main/java/com/newproject/etf/controller/EtfController.java
@@ -6,21 +6,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @Controller
 @RequestMapping("/openDataApi")
 public class EtfController {
     private final EtfService etfService;
-    private final String url = "https://apis.data.go.kr/1160100/service/GetSecuritiesProductInfoService";
-    private final String serviceKey = "BBDYHxpLb5iDQfFrXs95dcZqTnYTBG%2B%2Bo6bPr0BC9bmIHnG5gB48wToN04d4DM8uRSj7m5ha1mQvRdLJ%2Fpss9Q%3D%3D";
 
     @Autowired
     public EtfController(EtfService etfService) {
         this.etfService = etfService;
     }
 
-    @GetMapping(value = "/{apiName}", produces = "application/json")
-    public String getPriceInfo(Model model, @PathVariable("apiName") String apiName) {
-        model.addAttribute("resultList", etfService.list(url, serviceKey, apiName));
+    @GetMapping(value = "/{apiName}")
+    public String getPriceInfo(Model model, @PathVariable("apiName") String apiName, @RequestParam Map<String, String> queryParams) {
+        model.addAttribute("resultList", etfService.list(apiName, queryParams));
         return "etf";
     }
 }

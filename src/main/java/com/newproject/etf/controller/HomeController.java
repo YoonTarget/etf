d65e6bcbf06b37ade19262dc35027fb7811f53ac
YoonTarget <igeek437@gmail.com>
2025-03-20 22:44:11 +0900

    Modify : 화면단 작업중

---
+++ src/main/java/com/newproject/etf/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.newproject.etf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index"; // templates/index.html을 반환
+    }
+}

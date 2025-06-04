b3b35705795eb2a641a6ec8f07109ae866b6ee8d
YoonTarget <igeek437@gmail.com>
2025-06-04 23:18:23 +0900

    Add : ETFEntity 추가

---
+++ src/main/java/com/newproject/etf/entity/EtfEntity.java
@@ -0,0 +1,75 @@
+package com.newproject.etf.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "etf_entity")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class EtfEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // ID
+
+    @Column(name = "flt_rt", precision = 5, scale = 2)
+    private BigDecimal fltRt; // 등락률
+
+    @Column(name = "nav", precision = 15, scale = 2)
+    private BigDecimal nav; // 순자산가치
+
+    @Column(name = "mkp", precision = 10, scale = 0)
+    private BigDecimal mkp; // 시가
+
+    @Column(name = "hipr", precision = 10, scale = 0)
+    private BigDecimal hipr; // 고가
+
+    @Column(name = "lopr", precision = 10, scale = 0)
+    private BigDecimal lopr; // 저가
+
+    @Column(name = "trqu")
+    private Long trqu; // 거래량
+
+    @Column(name = "tr_prc")
+    private Long trPrc; // 거래대금
+
+    @Column(name = "mrkt_tot_amt")
+    private Long mrktTotAmt; // 시가총액
+
+    @Column(name = "n_ppt_tot_amt")
+    private Long nPptTotAmt; // 순자산총액
+
+    @Column(name = "st_lstg_cnt")
+    private Long stLstgCnt; // 상장좌수
+
+    @Column(name = "bss_idx_idx_nm", length = 240)
+    private String bssIdxIdxNm; // 기초지수_지수명
+
+    @Column(name = "bss_idx_clpr", precision = 10, scale = 3)
+    private BigDecimal bssIdxClpr; // 기초지수_종가
+
+    @Column(name = "bas_dt")
+    private LocalDate basDt; // 기준일자
+
+    @Column(name = "srtn_cd", length = 6)
+    private String srtnCd; // 단축코드
+
+    @Column(name = "isin_cd", length = 12)
+    private String isinCd; // ISIN코드
+
+    @Column(name = "itms_nm", length = 120)
+    private String itmsNm; // 종목명
+
+    @Column(name = "clpr", precision = 10, scale = 0)
+    private BigDecimal clpr; // 종가
+
+    @Column(name = "vs", precision = 10, scale = 0)
+    private BigDecimal vs; // 대비
+}

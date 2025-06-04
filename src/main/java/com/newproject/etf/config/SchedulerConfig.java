3d5ef94b1cac3bd15a90df5c4c4c4de27e73f8c6
YoonTarget <igeek437@gmail.com>
2025-06-04 23:18:57 +0900

    Add : SchedulerConfig 추가

---
+++ src/main/java/com/newproject/etf/config/SchedulerConfig.java
@@ -0,0 +1,9 @@
+package com.newproject.etf.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulerConfig {
+}

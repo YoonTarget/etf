f496a0498f1d76097f808856a67b733f18a1009e
YoonTarget <igeek437@gmail.com>
2025-06-14 01:11:01 +0900

    Add : EtfDto 추가

---
+++ src/main/java/com/newproject/etf/dto/EtfDto.java
@@ -0,0 +1,11 @@
+package com.newproject.etf.dto;
+
+import lombok.Data;
+
+@Data
+public class EtfDto {
+    private String numOfRows = "10";
+    private String pageNo = "1";
+    private String likeItmsNm = "";
+    private String basDt = "";
+}

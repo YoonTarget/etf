4131aec23ce02e42fcc2453dd767e78857b9bd3c
YoonTarget <igeek437@gmail.com>
2025-06-17 23:39:54 +0900

    Modify : 스케쥴러 작업중

--- src/main/java/com/newproject/etf/scheduler/ApiDataScheduler.java
+++ src/main/java/com/newproject/etf/scheduler/ApiDataScheduler.java
@@ -4,6 +4,8 @@ import com.newproject.etf.service.EtfService;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
+
 @Component
 public class ApiDataScheduler {
 
@@ -13,8 +15,15 @@ public class ApiDataScheduler {
         this.etfService = etfService;
     }
 
-    @Scheduled(cron = "0 0 0 * * ?") // 매일 자정 실행
+//    @Scheduled(cron = "0 0 0 * * ?") // 매일 자정 실행
     public void updateEtfData() {
-//        etfService.updateDailyData();
+        String endPoint = "getETFPriceInfo";
+        Map<String, String> queryParams = Map.of(
+                "numOfRows", "1000",
+                "pageNo", "1",
+                "beginBasDt", "20230101",
+                "endBasDt", "20231231"
+        );
+        etfService.list(endPoint, queryParams);
     }
 }

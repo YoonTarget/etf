8ea953be418e0f268089780f9784361662d0306e
YoonTarget <igeek437@gmail.com>
2025-11-10 22:58:26 +0900

    Fix : 기초지수 관련 데이터 추가 & 상장일 제외 타 데이터는 최신 데이터 노출

--- src/main/java/com/newproject/etf/dto/EtfInfoView.java
+++ src/main/java/com/newproject/etf/dto/EtfInfoView.java
@@ -2,10 +2,12 @@ package com.newproject.etf.dto;
 
 // 표시 전용
 public record EtfInfoView(
-        String itmsNm,
-        String isinCd,
-        String basDt,       // yyyy.MM.dd 형식 문자열
-        String nav,         // "12,345.67원"
-        String mrktTotAmt   // "1.23조 원" 또는 "123억 원"
+        String itmsNm, // 종목명
+        String isinCd, // ISIN코드
+        String bssIdxIdxNm, // 기초지수_지수명
+        String bssIdxClpr, // 기초지수_종가
+        String mrktTotAmt, // 시가총액("1.23조 원" 또는 "123억 원")
+        String nav, // 순자산가치("12,345.67원")
+        String basDt // 기준일자(yyyy.MM.dd 형식 문자열)
 ) {}
 
